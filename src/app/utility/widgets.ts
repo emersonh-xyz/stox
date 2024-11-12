71835016880518f9258303def16c6e34fd2a9783
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-11-12 12:29:47 -0500

    Update widgets.ts

--- src/app/utility/widgets.ts
+++ src/app/utility/widgets.ts
@@ -1,3 +1,13 @@
+export type Stock = {
+    currency?: string,
+    description: string,
+    displaySymbol?: string,
+    figi?: string,
+    symbol: string,
+    type?: string,
+    quote?: any,
+}
+
 export async function toggleWidget(componentIndex: number) {
     const components = JSON.parse(localStorage.getItem('components') || '[]');
     components[componentIndex] = !components[componentIndex];
@@ -10,3 +20,12 @@ export async function getWidgets() {
     return JSON.parse(localStorage.getItem('components') || '[]');
 }
 
+
+export const fetchStockQuote = async (symbol: string) => {
+    const res = await fetch(`/api/finnhub/quote`, {
+        method: 'POST',
+        body: JSON.stringify({ symbol }),
+    })
+    const data = await res.json()
+    return data;
+}
