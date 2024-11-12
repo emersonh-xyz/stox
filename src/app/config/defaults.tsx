1ceb7d02e264950788fab32f9e10864def73061d
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-11-12 11:42:44 -0500

    Create defaults.tsx

---
+++ src/app/config/defaults.tsx
@@ -0,0 +1,28 @@
+import WatchList from "@/components/Widgets/WatchList"
+import { Watch } from "lucide-react"
+
+export const watchListDefaults = [
+    {
+        'symbol': 'AAPL',
+        'description': 'Apple Inc.',
+    },
+    {
+        'symbol': 'MSFT',
+        'description': 'Microsoft Corporation'
+    },
+    {
+        'symbol': 'TSLA',
+        'description': 'Tesla Inc.'
+
+    },
+    {
+        'symbol': 'AMZN',
+        'description': 'Amazon.com Inc.'
+    },
+    {
+        'symbol': 'NFLX',
+        'description': 'Netflix Inc.'
+    }
+]
+
+
