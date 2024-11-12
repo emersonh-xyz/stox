36dbe7272def58735ccd6f8b6d6e4824f89604d6
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-11-12 09:28:43 -0500

    Update route.ts

--- src/app/api/finnhub/quote/route.ts
+++ src/app/api/finnhub/quote/route.ts
@@ -1,13 +1,23 @@
 import { getStockQuote } from "@/app/utility/finnhub"
-import { headers } from 'next/headers'
+import { headers } from "next/headers";
+import { NextRequest } from "next/server"
 
 export const dynamic = 'force-static'
 
-export async function GET() {
+export async function POST(req: Request) {
 
+    const { symbol } = await req.json()
+    console.log(symbol)
 
+    if (!symbol) {
+        return new Response('Symbol is required', {
+            status
+                : 400
+        });
+    }
 
+    const quote = await getStockQuote(symbol);
 
-    // const quote = await getStockQuote(symbol)
-    // return Response.json({ quote })
+    return new Response(JSON.stringify(quote));
+
 }
