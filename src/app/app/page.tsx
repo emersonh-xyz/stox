825b9c2924460a53d154163f002825074cb41135
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2024-11-12 09:29:14 -0500

    WidgetAdder on dashboard only

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,6 @@
 import WatchList from "@/components/Widgets/WatchList";
 import { getAllStocks } from "../utility/finnhub";
+import WidgetAdder from "@/components/WidgetAdder";
 
 export default async function Dashboard({
     params,
@@ -12,8 +13,13 @@ export default async function Dashboard({
     const stocks = await getAllStocks();
 
     return (
-        <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-3 ">
-            <WatchList stocks={stocks} />
+        <div className="flex justify-between ">
+            <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-3 ">
+                <WatchList stocks={stocks} />
+            </div>
+            <div className="px-24">
+                <WidgetAdder />
+            </div>
         </div>
     )
 }
