77b5cb96414c59e34c1327dd667f8539a292dd6a
Emerson <emersonhicks003@gmail.com>
2024-11-13 21:47:09 -0500

    Update page.tsx

--- src/app/app/social/page.tsx
+++ src/app/app/social/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { getLocaleLanguage } from '@/app/utility/language';
+import { Stock } from '@/app/utility/widgets';
 import SocialFeed from '@/components/Widgets/SocialFeed';
 import { Search, ThumbsUp } from "lucide-react";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export type SocialData = {
     avatar: string;
@@ -14,10 +16,27 @@ export type SocialData = {
 
 export default function SocialPage() {
 
+    const [stocksData, setStocksData] = useState<Stock[]>([]);
+
+    const [language, setLanguage] = useState('en');
+
+    useEffect(() => {
+        const fetchStocksData = async () => {
+            const res = await fetch(`/api/internal/stocks`, {
+                method: 'GET',
+            });
+            const data = await res.json();
+            setStocksData(data);
+        }
+
+        setLanguage(getLocaleLanguage());
+
+        fetchStocksData();
+    }, [])
 
     return (
         <div className="grid grid-cols-1 gap-20 md:grid-cols-2 lg:grid-cols-3">
-
+            <SocialFeed data={stocksData} lang={language} />
         </div>
     );
 }
